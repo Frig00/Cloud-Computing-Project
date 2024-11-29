64ef133e984eead52d2786327e30f559c1a20276
Davide Ferrari <xthehacker2000x@gmail.com>
2024-11-29 12:07:16 +0100

    🩹 Fix controller not reachable from Docker container

--- src/controller/src/index.ts
+++ src/controller/src/index.ts
@@ -20,7 +20,7 @@ const startServer = async () => {
     registerRoutes(fastify);
 
   
-    await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+    await fastify.listen({ port: Number(process.env.PORT) || 3000 , host: "0.0.0.0"});
     console.log(
       `Server running at http://localhost:${process.env.PORT || 3000}`,
     );
