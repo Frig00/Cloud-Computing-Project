4fe7088d0477786a420722ea9c2db9bf7045653a
alerotta <alerotta@yahoo.it>
2024-12-09 14:52:20 +0100

    🥷  /search minor fix

--- src/frontend/src/components/Search.tsx
+++ src/frontend/src/components/Search.tsx
@@ -3,20 +3,29 @@ import VideoThumbnail from "./VideoThumbnail";
 import { useQuery } from "@tanstack/react-query";
 import { VideoApi } from "@/api";
 import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { thumbnailSrc } from "../lib/consts";
 
 export default function Search() {
+
+
   const [searchParams] = useSearchParams();
   const searchedTitle = searchParams.get("q")!;
-
   const videoApi = new VideoApi();
 
-  const { isPending, error, data } = useQuery({
+  const { isPending, error, data,refetch } = useQuery({
     queryKey: ["videoSearchPost"],
     queryFn: () =>
       videoApi.videoSearchPost({
         q: searchedTitle,
       }),
   });
+
+  useEffect(() => {
+    if (searchedTitle) {
+      refetch();
+    }
+  }, [searchedTitle, refetch]);
 
   if (isPending) return "Loading...";
 
@@ -28,9 +37,9 @@ export default function Search() {
         {data.map((video) => (
           <VideoThumbnail
             id={video.id}
-            src={""}
+            src={thumbnailSrc(video.id)}
             title={video.title}
-            user={""}
+            user={video.userId}
             key={video.id}
             variant="horizontal"
           />
