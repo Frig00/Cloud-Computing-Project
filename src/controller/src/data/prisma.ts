15b1645adc31bd95693f5e39595b80493fac6498
Davide Ferrari <xthehacker2000x@gmail.com>
2024-12-14 17:26:20 +0100

    🗃️ Updates database connection configuration to fix Docker compose issue

--- src/controller/src/data/prisma.ts
+++ src/controller/src/data/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DB_CONNECTION,
+    },
+  },
+});
 
 export default prisma;
