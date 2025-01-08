18d0047ea93b1491d992d35b11c6710854050558
Davide Ferrari <xthehacker2000x@gmail.com>
2025-01-08 10:58:47 +0100

    🩹 Fix broken git merge

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# resource "aws_s3_bucket" "example_bucket" {
-#   bucket = "${var.project_name}-${var.bucket_name}"
-# }
-=======
 resource "aws_s3_bucket" "video_bucket" {
   bucket = "${var.project_name}-${var.bucket_name}"
 }
->>>>>>> ff5db9b (🔧Add terraform files for database)
