725bab23f7d5b1d18d4cbba864bd650f2804903a
Andrea Frigatti <andrea.frigatti01@universitadipavia.it>
2025-02-04 17:58:04 +0100

    🔧 Update CORS configuration to allow GET method for S3 bucket

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_cors_configuration" "video_bucket_cors" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
+    allowed_methods = ["PUT", "POST", "GET"]
     allowed_origins = ["*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
